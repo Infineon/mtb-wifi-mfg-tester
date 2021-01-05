9483860e5f5c97317472d4b4eea00e5639cc475b
gitlab-runner <do-not-reply@cypress.com>
2021-01-04 23:32:50 -0600

    Upload mtb-anycloud-wifi-mfg-tester 3.0.0.36

--- README.md
+++ README.md
@@ -1,1 +1,109 @@
-# mfg-test-anycloud
+# PSoC 6 MCU: WLAN Manufacturing Test Application for FreeRTOS
+
+The WLAN Manufacturing Test Application is used to validate the WLAN Firmware and Radio Performance of Wifi Chip.
+
+The WLAN MFG Application acts as a transport layer between the host "wl tool" and WLAN Firmware. Mfg Test Application recieves commands from the wl tool and forwards them to the Wlan firmware using IOVAR/IOCTL commands. The Mfg Test App also relays the response received back from Wlan firmware.
+
+The "wl tool" binaries for testing the WLAN Firmware are also included in this application repository. The example of using this application with wl tool is shown below.
+
+
+## Requirements
+- [ModusToolbox™ IDE](https://www.cypress.com/products/modustoolbox-software-environment) v2.2
+- Programming Language: C
+- Supported Toolchains: Arm® GCC, IAR
+- Associated Parts: All [PSoC 6 MCU](http://www.cypress.com/PSoC6) parts
+
+## Dependent assets
+- [Wifi Connection Manager](https://github.com/cypresssemiconductorco/wifi-connection-manager)
+- [Wifi Manufacturing Test Library](https://github.com/cypresssemiconductorco/wifi-mfg-test)
+
+
+## Validated Kits
+- [PSoC 6 Wi-Fi BT Prototyping Kit](https://www.cypress.com/CY8CPROTO-062-4343W) (CY8CPROTO-062-4343W) - Default target
+- [PSoC 62S2 Wi-Fi BT Pioneer Kit](https://www.cypress.com/CY8CKIT-062S2-43012) (CY8CKIT-062S2-43012)
+- [PSoC 6 WiFi-BT Pioneer Kit](https://www.cypress.com/CY8CKIT-062-WIFI-BT) (CY8CKIT-062-WIFI-BT)
+- [PSoC 62S1 WiFi-BT Pioneer Kit](https://www.cypress.com/CYW9P62S1-43438EVB-01) (CYW9P62S1-43438EVB-01)
+
+
+## Hardware Setup
+
+This application uses the board's default configuration. See the kit user guide to ensure that the board is configured correctly.
+
+The application running on a PSoC 6 MCU kit and the test setup are shown below:
+
+![Mfg test setup](mfg-test-setup.png)
+
+Note: The PSoC 6 BLE Pioneer Kit (CY8CKIT-062-BLE) and the PSoC 6 WiFi-BT Pioneer Kit (CY8CKIT-062-WIFI-BT) ship with KitProg2 installed. ModusToolbox software requires KitProg3. Before using this application, make sure that the board is upgraded to KitProg3. The tool and instructions are available in the Firmware Loader GitHub repository. If you do not upgrade, you will see an error like "unable to find CMSIS-DAP device" or "KitProg firmware is out of date".
+
+
+## Software Setup
+
+This application requires WL Tool running on a windows PC and uses UART port for communication with target. The pre-built executables for WL Tool are available in wl-tool-bin/ directory.
+
+
+## Using the Application
+
+
+### In Command-line Interface (CLI) on the build machine:
+
+
+#### Building app
+
+On Linux and macOS, you can use any terminal application. On Windows, open the modus-shell app from the Start menu.
+
+- `git clone https://github.com/cypresssemiconductorco/mtb-anycloud-wifi-mfg-tester`
+- `cd mtb-anycloud-wifi-mfg-tester`
+- `make getlibs`
+- `make program TARGET=CY8CPROTO-062-4343W TOOLCHAIN=GCC_ARM`
+
+
+## Operation
+
+1. Go to wl tool directory using `cd wl-tool-bin/`
+
+2. Reset the Board by pressing SW1 button
+
+3. Run the command on Windows Host for the proper Wlan Chip on target board.
+   wl4343x.exe --serial `port` ver
+   #example wl4343x.exe --serial 139 ver
+
+4. Observe the output of the command
+
+5. List of WL commands which can be retrieved by typing --help
+   Partial output of the command and display is below.
+
+   #example wl4343x.exe --serial 139 --help
+
+   Usage: wl4343WA1.exe [-a|i <adapter>] [-h] [-d|u|x] <command> [arguments]
+
+		-h        this message and command descriptions
+		-h [cmd]  command description for cmd
+		-a, -i    adapter name or number
+		-d        output format signed integer
+		-u        output format unsigned integer
+		-x        output format hexdecimal
+
+		ver     get version information
+
+		cmds    generate a short list of available commands
+
+		ioctl_echo
+			check ioctl functionality
+
+		up      reinitialize and mark adapter up (operational)
+
+		down    reset and mark adapter down (disabled)
+
+		out     mark adapter down but do not reset hardware(disabled)
+				On dualband cards, cards must be bandlocked before use.
+
+		clk     set board clock state. return error for set_clk attempt if the driver is not down
+				0: clock off
+				1: clock on
+
+		restart Restart driver.  Driver must already be down.
+
+		reboot  Reboot platform
+
+
+Note: For CYW4343W and CYW43438 based targets, please use the wl tool: wl4343X.exe
---
+++ src/main.c
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2020, Cypress Semiconductor Corporation or a subsidiary of
+ * Cypress Semiconductor Corporation. All Rights Reserved.
+ *
+ * This software, including source code, documentation and related
+ * materials ("Software"), is owned by Cypress Semiconductor Corporation
+ * or one of its subsidiaries ("Cypress") and is protected by and subject to
+ * worldwide patent protection (United States and foreign),
+ * United States copyright laws and international treaty provisions.
+ * Therefore, you may use this Software only as provided in the license
+ * agreement accompanying the software package from which you
+ * obtained this Software ("EULA").
+ * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+ * non-transferable license to copy, modify, and compile the Software
+ * source code solely for use in connection with Cypress's
+ * integrated circuit products. Any reproduction, modification, translation,
+ * compilation, or representation of this Software except as specified
+ * above is prohibited without the express written permission of Cypress.
+ *
+ * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+ * reserves the right to make changes to the Software without notice. Cypress
+ * does not assume any liability arising out of the application or use of the
+ * Software or any product or circuit described in the Software. Cypress does
+ * not authorize its products for use in any products where a malfunction or
+ * failure of the Cypress product may reasonably be expected to result in
+ * significant property damage, injury or death ("High Risk Product"). By
+ * including Cypress's product in a High Risk Product, the manufacturer
+ * of such system or application assumes all risk of such use and in doing
+ * so agrees to indemnify Cypress against all liability.
+ */
+ /** @file
+ *
+ * Description: This is the source code for mfg_test App Example in ModusToolbox.
+ *
+ * Related Document: See README.md
+  *
+ */
+
+/* Header file includes */
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "cy_wcm.h"
+#include "cy_lwip.h"
+
+#include "mfg_test_common_api.h"
+
+/******************************************************************************
+* Macros
+******************************************************************************/
+
+/* Task parameters for MfgTest App Task. */
+#define MFG_TEST_CLIENT_TASK_PRIORITY       (2)
+#define MFG_TEST_CLIENT_TASK_STACK_SIZE     (1024 * 2)
+
+#define WCM_INITIALIZED                  (1lu << 0)
+
+/* Macro to check if the result of an operation was successful  When
+ * it has failed, print the error message and return EXIT_FAILURE to the
+ * calling function.
+ */
+#define CHECK_RESULT(result, init_mask, error_message...)   \
+                     do                                     \
+                     {                                      \
+                         if ((int)result != EXIT_SUCCESS)   \
+                         {                                  \
+                             printf(error_message);         \
+                             return;                        \
+                         }                                  \
+                     } while(0)
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+cy_rslt_t cy_wcm_get_whd_interface(cy_wcm_interface_t interface_type, whd_interface_t *whd_iface);
+static void mfg_test_client_task(void *pvParameters);
+
+/******************************************************************************
+* Global Variables
+******************************************************************************/
+/* This enables RTOS aware debugging. */
+volatile int uxTopUsedPriority;
+
+#define IOCTL_MED_LEN   (512)
+static unsigned char buf[IOCTL_MED_LEN] = {0};
+
+/*
+ * This is called first after the initialization of FreeRTOS.  It basically connects to WiFi and then starts the
+ * task that waits for DHCP.  No tasks will actually run until this function returns.
+ */
+void vApplicationDaemonTaskStartupHook()
+{
+    printf("Mfg test Application.\r\n");
+
+    /* Create the mfg_test Client task. */
+    xTaskCreate(mfg_test_client_task, "Mfg-test task", MFG_TEST_CLIENT_TASK_STACK_SIZE,
+                NULL, MFG_TEST_CLIENT_TASK_PRIORITY, NULL);
+
+}
+
+/************************************************************************************
+* Main initializes the hardware and low power support, and starts the task scheduler
+*************************************************************************************/
+int main()
+{
+    cy_rslt_t result ;
+
+    /* This enables RTOS aware debugging in OpenOCD */
+    uxTopUsedPriority = configMAX_PRIORITIES - 1;
+
+    /* Initialize the board support package */
+    result = cybsp_init() ;
+    CY_ASSERT(result == CY_RSLT_SUCCESS);
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    /* Initialize retarget-io to use the debug UART port */
+    cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
+
+    /* clear the screen */
+    printf("\x1b[2J\x1b[;H");
+    printf("=============================================\r\n");
+    printf("FreeRTOS MfgTest Application\r\n") ;
+    printf("=============================================\r\n");
+
+    /* Start the FreeRTOS scheduler */
+    vTaskStartScheduler() ;
+
+    /* Should never get here */
+    CY_ASSERT(0) ;
+}
+
+/******************************************************************************
+ * Function Name: mfg_test_client_task
+ ******************************************************************************
+ * Summary:
+ *  Task for handling initialization & connection of Wi-Fi and the MQTT client.
+ *
+ * Parameters:
+ *  void *pvParameters : Task parameter defined during task creation (unused)
+ *
+ * Return:
+ *  void
+ *
+ ******************************************************************************/
+static void mfg_test_client_task(void *pvParameters)
+{
+    cy_wcm_config_t config = {.interface = CY_WCM_INTERFACE_TYPE_STA};
+    cy_rslt_t result;
+    whd_interface_t sta_interface = NULL;
+
+    /* To avoid compiler warnings */
+    (void)pvParameters;
+
+    /* Set I/O to No Buffering */
+    setvbuf( stdin, NULL, _IONBF, 0 );
+
+    /* Set I/O to No Buffering */
+    setvbuf( stdout, NULL, _IONBF, 0 );
+
+    /* Configure the interface as a Wi-Fi STA (i.e. Client). */
+
+    /* Initialize the Wi-Fi Connection Manager and return if the operation fails. */
+    result = cy_wcm_init(&config);
+
+    CHECK_RESULT(result, WCM_INITIALIZED, "\r\nWi-Fi Connection Manager initialization failed!\r\n");
+
+    result = cy_wcm_get_whd_interface(CY_WCM_INTERFACE_TYPE_STA, &sta_interface);
+    if (result != CY_RSLT_SUCCESS) {
+        printf("\r\nWifi STA Interface Not found, Wi-Fi Connection Manager initialization failed!\r\n");
+        return;
+    }
+
+    wl_set_sta_interface_handle(sta_interface);
+
+    printf("\r\nWi-Fi Connection Manager initialized.\r\n");
+
+    while (true)
+    {
+        memset(buf, 0, sizeof(buf));
+        wl_remote_command_handler( buf);
+    }
+
+    /* Cleanup section for various operations. */
+    vTaskDelete( NULL );
+}
+
+/* [] END OF FILE */
